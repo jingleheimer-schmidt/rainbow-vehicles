2c0806284ad8466baae00ffa063cd42763097207
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2025-07-15 22:16:29 -0700

    formatting

--- settings.lua
+++ settings.lua
@@ -1,33 +1,34 @@
+
 local rainbowSpeedSetting = {
-  type = "string-setting",
-  name = "vehicle-rainbow-speed",
-  setting_type = "runtime-global",
-  default_value = "default",
-  allowed_values = {
-    "off",
-    "veryslow",
-    "slow",
-    "default",
-    "fast",
-    "veryfast"
-  }
+    type = "string-setting",
+    name = "vehicle-rainbow-speed",
+    setting_type = "runtime-global",
+    default_value = "default",
+    allowed_values = {
+        "off",
+        "veryslow",
+        "slow",
+        "default",
+        "fast",
+        "veryfast"
+    }
 }
 
 local rainbowPaletteSetting = {
-  type = "string-setting",
-  name = "vehicle-rainbow-palette",
-  setting_type = "runtime-global",
-  default_value = "default",
-  allowed_values = {
-    "pastel",
-    "light",
-    "default",
-    "vibrant",
-    "deep"
-  }
+    type = "string-setting",
+    name = "vehicle-rainbow-palette",
+    setting_type = "runtime-global",
+    default_value = "default",
+    allowed_values = {
+        "pastel",
+        "light",
+        "default",
+        "vibrant",
+        "deep"
+    }
 }
 
 data:extend({
-  rainbowSpeedSetting,
-  rainbowPaletteSetting
+    rainbowSpeedSetting,
+    rainbowPaletteSetting
 })
